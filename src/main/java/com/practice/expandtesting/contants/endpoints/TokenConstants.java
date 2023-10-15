d6ac77940ecc983e94294c52f67ed9bd78fe1d49
Fabricia <fabricia.mathes@gmail.com>
2023-10-15 20:47:57 -0300

    Implementado cenário de teste DeleteUsersTest.

---
+++ src/main/java/com/practice/expandtesting/contants/endpoints/TokenConstants.java
@@ -0,0 +1,5 @@
+package com.practice.expandtesting.contants.endpoints;
+
+public class TokenConstants {
+    public static final String INCORRECT_TOKEN = "bdd537a15fb6460099b33c736c097dea53a31b6829f24b80ade358b04e92cb20";
+}
